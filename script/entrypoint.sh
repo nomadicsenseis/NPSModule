5a8cba13a14e28d050c49c2d348bff4b247e0eb2
NPSModule <action@github.com>
2025-11-11 11:19:48 +0000

    Improve entrypoint.sh with better logging

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Generating Catia report.\n"
+echo -e "Generating Catia report...\n"
 
-python dashboard_analyzer/weekly_deep_research.py
+python -u dashboard_analyzer/weekly_deep_research.py
+
+echo -e "\nCatia report generation completed."
